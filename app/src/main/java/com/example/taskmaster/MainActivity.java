c2773acf4292813d0228f6031118e6b48b15c44a
daesy <daesyramos@hotmail.com>
2020-02-11 13:48:35 -0800

    task manager

--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // GO TO ADD TASK
+        Button goToAddTaskPage = findViewById(R.id.button);
+        goToAddTaskPage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent goToAddTask = new Intent(MainActivity.this, AddTask.class);
+                MainActivity.this.startActivity(goToAddTask);
+            }
+        });
+
+
+        // GO TO ALL TASK
+        Button goToAllTaskPage = findViewById(R.id.button2);
+        goToAllTaskPage.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent goToAllTask = new Intent(MainActivity.this, AllTask.class);
+                MainActivity.this.startActivity(goToAllTask);
+            }
+        });
     }
 }
