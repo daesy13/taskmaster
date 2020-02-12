7a0777d5c5af2567135665924648b8daf4b78685
daesy <daesyramos@hotmail.com>
2020-02-12 13:53:15 -0800

    tuesdayLab

--- app/src/main/java/com/daesy/taskmaster/AllTask.java
+++ app/src/main/java/com/daesy/taskmaster/AllTask.java
@@ -1,14 +1,14 @@
-package com.example.taskmaster;
+package com.daesy.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class AddTask extends AppCompatActivity {
+public class AllTask extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_task);
+        setContentView(R.layout.activity_all_task);
     }
 }
